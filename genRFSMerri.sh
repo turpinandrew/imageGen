947cf9037b35cfdd041be590ea7a60b6c7eebc3a
Andrew Turpin <aturpin@unimelb.edu.au>
2013-01-23 04:26:46 +1100

    Broke into colelctions of  * 2 jobs

--- genRFSMerri.sh
+++ genRFSMerri.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 
-rootDir=/vlsci/VR0052/aturpin/doc/papers/merp/src/imageGen/RFS_Aspect
+#rootDir=/vlsci/VR0052/aturpin/doc/papers/merp/src/imageGen/RFS_Aspect
+rootDir=/vlsci/VR0052/aturpin/doc/papers/merp/src/imageGen/RFS_Wide
 
 \rm -rf $rootDir
 mkdir $rootDir
 
 n=1
-levels="02" #  04 08 16 32"
+levels="02"  #  04 08 16 32"
 
 seq=`awk 'BEGIN{for(i=1;i<='"$n"';i++) print i; exit}'`
 
@@ -15,21 +16,26 @@ seq=`awk 'BEGIN{for(i=1;i<='"$n"';i++) print i; exit}'`
 # create and submit pbs script
 #   $1 == script file name
 #   $2 == job name
-#   $3 == level
-#   $4 == target RF
-#   $5 == distractor RF
-#   $6 == radius
-#   $7 == output file
+#   $3 == output dir
 ########################
 function createScript() {
     echo "#!/bin/bash"                               >  $1
-    echo "#PBS -l procs=1"                           >> $1
-    echo "#PBS -l walltime=:00:2:00"                 >> $1
+    echo "#PBS -l procs=8"                           >> $1
+    echo "#PBS -l walltime=:00:10:00"                >> $1
     echo "#PBS -N $2"                                >> $1
     echo "module load R-gcc/2.15.0"                  >> $1
     echo "module load imagemagick/6.6.5-10"          >> $1
-    echo "R --slave --args $3 $4 $5 $6 < RFS.r > $7" >> $1
-    echo "mogrify $7 `echo $7 | sed 's/pgm/png/'`  " >> $1
+    echo "w="                                        >> $1
+    for j in $levels
+    do
+      echo "R --slave --args $j 3 4 1 < RFS.r > $3/s"$j"_"$i".pgm &" >> $1
+      echo "w=\"\$w  \$!\"" >> $1
+      echo "R --slave --args $j 4 4 1 < RFS.r > $3/n"$j"_"$i".pgm &" >> $1
+      echo "w=\"\$w  \$!\"" >> $1
+    done
+    echo "wait \$w" >> $1
+    echo "mogrify -format png $3/*.pgm             " >> $1
+    #echo "rm $3/*.pgm                              " >> $1
     qsub -d `pwd` $1
 }
 
@@ -39,9 +45,5 @@ function createScript() {
 for i in $seq
 do
     echo "Number $i"
-    for j in $levels
-    do
-        createScript xrfs.sh xrfs$j"_"$i $j 3 4 1 $rootDir/s"$j"_"$i".pgm
-        createScript xrfs.sh xrfs$j"_"$i $j 4 4 1 $rootDir/n"$j"_"$i".pgm
-    done
+    createScript xrfs.sh xrfs$i $rootDir
 done
