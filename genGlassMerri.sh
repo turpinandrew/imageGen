37102fa2c2173dff23313842103909734c7be415
Andrew Turpin <aturpin@unimelb.edu.au>
2013-01-23 21:46:29 +1100

    minor edits

--- genGlassMerri.sh
+++ genGlassMerri.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-root=/vlsci/VR0052/aturpin/doc/papers/merp/src/imageGen/Glass
-sDir=signal     # signal images in    $root/$sDir
-nDir=nosignal   # no signal images in $root/$nDir
-n=200           # number of each
-levels="0.08 0.16 0.24 0.32 0.40 0.48 0.52"
+root=/vlsci/VR0052/aturpin/doc/papers/merp/src/imageGen/Glass_temp
+n=3           # number of each
+#levels="0.08 0.16 0.24 0.32 0.40 0.48 0.52"
+levels="0.80"
 
 ##################################
 # mkdirs
 ##################################
 \rm -rf $root
 mkdir $root
-mkdir $root/$sDir
-mkdir $root/$nDir
 
 seq=`awk 'BEGIN{for(i=1;i<='"$n"';i++) printf"%03d ",i; exit}'`
 
@@ -21,6 +18,7 @@ seq=`awk 'BEGIN{for(i=1;i<='"$n"';i++) printf"%03d ",i; exit}'`
 ########################
 for f in $levels
 do
+    mkdir $root/$f
     echo "Doing $f"
     for i in $seq
     do
@@ -30,7 +28,9 @@ do
         echo "#PBS -l walltime=:00:10:00" >> xglass.sh
         echo "#PBS -N xglass$f"_"$i" >> xglass.sh
         echo "module load R-gcc/2.15.0" >> xglass.sh
-        echo "R --slave --args $f a < glass.r > $root/$sDir/"$f"_"$i".pbm" >> xglass.sh
+        echo "module load imagemagick/6.6.5-10" >> xglass.sh
+        echo "R --slave --args $f a < glass.r > $root/$f/$i.pbm" >> xglass.sh
+        echo "mogrify -format png $root/$f/$i.pbm" >> xglass.sh
         qsub -d `pwd` xglass.sh
     done
 done
@@ -38,14 +38,17 @@ done
 ###########################
 # make 0 signal patches
 ###########################
-for i in $seq
-do
-    echo -n " $i"
-    echo "#!/bin/bash" > xglass.sh
-    echo "#PBS -l procs=1" >> xglass.sh
-    echo "#PBS -l walltime=:00:10:00" >> xglass.sh
-    echo "#PBS -N xglass$f"_"$i" >> xglass.sh
-    echo "module load R-gcc/2.15.0" >> xglass.sh
-    echo "R --slave --args 0 a < glass.r > $root/$nDir/n_"$i".pbm" >> xglass.sh
-    qsub -d `pwd` xglass.sh
-done
+#mkdir $root/noise
+#for i in $seq
+#do
+#    echo -n " $i"
+#    echo "#!/bin/bash" > xglass.sh
+#    echo "#PBS -l procs=1" >> xglass.sh
+#    echo "#PBS -l walltime=:00:10:00" >> xglass.sh
+#    echo "#PBS -N xglass0"_"$i" >> xglass.sh
+#    echo "module load R-gcc/2.15.0" >> xglass.sh
+#    echo "module load imagemagick/6.6.5-10" >> xglass.sh
+#    echo "R --slave --args 0 a < glass.r > $root/noise/$i.pbm" >> xglass.sh
+#    echo "mogrify -format png $root/noise/$i.pbm" >> xglass.sh
+#    qsub -d `pwd` xglass.sh
+#done
--- genRFSMerri.sh
+++ genRFSMerri.sh
@@ -7,8 +7,8 @@ rootDir=/vlsci/VR0052/aturpin/doc/papers/merp/src/imageGen/RFS_Wide
 \rm -rf $rootDir
 mkdir $rootDir
 
-n=1
-levels="02"  #  04 08 16 32"
+n=200
+levels="02 04 08 16 32"
 
 seq=`awk 'BEGIN{for(i=1;i<='"$n"';i++) print i; exit}'`
 
@@ -20,21 +20,17 @@ seq=`awk 'BEGIN{for(i=1;i<='"$n"';i++) print i; exit}'`
 ########################
 function createScript() {
     echo "#!/bin/bash"                               >  $1
-    echo "#PBS -l procs=8"                           >> $1
-    echo "#PBS -l walltime=:00:10:00"                >> $1
+    echo "#PBS -l procs=1"                           >> $1
+    echo "#PBS -l walltime=:00:15:00"                >> $1
     echo "#PBS -N $2"                                >> $1
     echo "module load R-gcc/2.15.0"                  >> $1
     echo "module load imagemagick/6.6.5-10"          >> $1
-    echo "w="                                        >> $1
     for j in $levels
     do
-      echo "R --slave --args $j 3 4 1 < RFS.r > $3/s"$j"_"$i".pgm &" >> $1
-      echo "w=\"\$w  \$!\"" >> $1
-      echo "R --slave --args $j 4 4 1 < RFS.r > $3/n"$j"_"$i".pgm &" >> $1
-      echo "w=\"\$w  \$!\"" >> $1
+      echo "R --slave --args $j 3 4 1 < RFS.r > $3/s"$j"_"$i".pgm " >> $1
+      echo "R --slave --args $j 4 4 1 < RFS.r > $3/n"$j"_"$i".pgm " >> $1
+      echo "mogrify -format png $3/*$j"_"$i.pgm" >> $1
     done
-    echo "wait \$w" >> $1
-    echo "mogrify -format png $3/*.pgm             " >> $1
     #echo "rm $3/*.pgm                              " >> $1
     qsub -d `pwd` $1
 }
